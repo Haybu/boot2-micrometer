cb222e29d0987bb11136e3da8dc13adf02d466a9
hxm3459 <haytham_mohamed@homedepot.com>
2018-02-26 23:41:44 -0600

    init

---
+++ src/main/java/io/agilehandy/mcm/Boot2MicrometerApplication.java
@@ -0,0 +1,12 @@
+package io.agilehandy.mcm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot2MicrometerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot2MicrometerApplication.class, args);
+	}
+}
